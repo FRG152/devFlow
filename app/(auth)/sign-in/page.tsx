fa19179c06bbfe8fdfa21ffd7514b8f92e493c34
FlavioRoman <romanflavio3@hotmail.com>
2025-09-17 13:56:08 -0300

    Created dynamic form for sign-in and sign-up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import ROUTES from "@/constants/routes";
+import { SignInSchema } from "@/lib/validations";
+import { success } from "zod";
+
 const SignIn = () => {
-  return <div></div>;
+  return (
+    <AuthForm
+      formType={ROUTES.SIGN_IN}
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import ROUTES from "@/constants/routes";
+import { SignUpSchema } from "@/lib/validations";
+
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <AuthForm
+      formType={ROUTES.SIGN_UP}
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignUp;
