e23403f92bbf655d9959ada11476e0103df540b6
FlavioRoman <romanflavio3@hotmail.com>
2025-09-17 12:45:19 -0300

    Implementation github authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div></div>;
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button>Log out</Button>
+      </form>
+    </>
+  );
 }
